9dd45b560f544f8a2aa47ff970866e33efe76c3c
ethanmcf <ethan_mcfarland@outlook.com>
2023-10-28 16:44:26 -0400

    Missed two files in previous api commit

--- src/data_access/api/SpotifyAPI.java
+++ src/data_access/api/SpotifyAPI.java
@@ -1,109 +1,117 @@
 package data_access.api;
 
-import data_access.FileAPIDataAccessObject;
+import entity.Song;
+import io.github.cdimascio.dotenv.Dotenv;
 import okhttp3.*;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Random;
 
-/**
- Example usage:
- SpotifyAPI sapi = new SpotifyAPI();
- ArrayList<HashMap> songs = sapi.getSongs();
- for(HashMap s: songs){
-    System.out.println(s);
- }
- **/
 public class SpotifyAPI{
-    private final FileAPIDataAccessObject APIDATA;
-    private final String GET_SEARCH_BASE_ENDPOINT;
-    private final String POST_AUTH_TOKEN_ENDPOINT;
-
+    private final String CLIENT_ID;
+    private final String CLIENT_SECRET;
+    private final OkHttpClient client;
+    private String authToken = null;
+    private String genre = "hiphop";
 
     public SpotifyAPI(){
-        this.GET_SEARCH_BASE_ENDPOINT = "https://api.spotify.com/v1/search?q=";
-        this.POST_AUTH_TOKEN_ENDPOINT = "https://accounts.spotify.com/api/token";
-        try {
-            this.APIDATA = new FileAPIDataAccessObject("./spotify_api.csv");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        Dotenv dotenv = Dotenv.configure().filename("api.env").load();
+        this.CLIENT_ID = dotenv.get("CLIENT_ID");
+        this.CLIENT_SECRET = dotenv.get("CLIENT_SECRET");
+        this.client = new OkHttpClient().newBuilder().build();
     }
-    public ArrayList<HashMap> getSongs(String genre){
-        return getSongsCall(genre);
+    public Song[] getSongs(String genre){
+        this.genre = genre;
+        return handleGetSongs();
     }
-    public ArrayList<HashMap> getSongs(){
-        return getSongsCall("hiphop");
+    public Song[] getSongs(){
+        return handleGetSongs();
     }
-    private ArrayList<HashMap> getSongsCall(String genre){
-        if(this.APIDATA.expiredToken()){
-            generateAuthToken();
-        }
-        Random r = new Random();
-        char search = (char) ('a' + r.nextInt(26));
-        String url = String.format("%s%s&genre:%s&type=track&limit=50",GET_SEARCH_BASE_ENDPOINT,search,genre);
-        OkHttpClient client = new OkHttpClient().newBuilder()
-                .build();
-        Request request = new Request.Builder()
-                .url(url)
-                .addHeader("Authorization", "Bearer " + APIDATA.getAuthToken())
-                .addHeader("Content-Type", "application/json")
-                .build();
-        ArrayList<HashMap> songs = new ArrayList<>();
-        try {
-            Response response = client.newCall(request).execute();
-
-            if (response.body() != null) {
-                JSONObject responseBody = new JSONObject(response.body().string());
-                JSONArray songsArray = responseBody.getJSONObject("tracks").getJSONArray("items");
-                for (int i = 0; i < songsArray.length(); i++) {
-                    JSONObject item = (JSONObject) songsArray.get(i);
-                    JSONObject albumArtistInfo = (JSONObject) item.getJSONObject("album").getJSONArray("artists").get(0);
-                    if(item.get("preview_url") instanceof String && item.getInt("popularity") > 73){
-                        HashMap<String, String> song = new HashMap<>();
-                        song.put("name", item.getString("name"));
-                        song.put("audio", item.getString("preview_url"));
-                        song.put("artist", albumArtistInfo.getString("name"));
-                        songs.add(song);
-                    }
-                }
-            } else {
-                System.out.println("No response body :(");
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return songs;
-    }
-
-
     private void generateAuthToken(){
-        System.out.println("GENERATING NEW TOKEN");
-        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        String endpoint = "https://accounts.spotify.com/api/token";
         RequestBody body = new FormBody.Builder()
-                .add("client_id", APIDATA.getClientId())
-                .add("client_secret", APIDATA.getClientSecret())
+                .add("client_id", CLIENT_ID)
+                .add("client_secret", CLIENT_SECRET)
                 .add("grant_type", "client_credentials")
                 .build();
         Request request = new Request.Builder()
-                .url(POST_AUTH_TOKEN_ENDPOINT)
+                .url(endpoint)
                 .addHeader("Content-Type", "application/x-www-form-urlencoded")
                 .post(body)
                 .build();
         try{
             Response response = client.newCall(request).execute();
-            if (response.body() != null) {
-                JSONObject responseBody = new JSONObject(response.body().string());
-                APIDATA.setNewToken(responseBody.getString("access_token"));
-            }else{
-                System.out.println("No response body");
-            }
+
+            if(response.body() == null) {throw new RuntimeException("Error - no response body");}
+
+            JSONObject responseBody = new JSONObject(response.body().string());
+            authToken = responseBody.getString("access_token");
+
         } catch (IOException e){
             throw new RuntimeException(e);
         }
     }
+    private JSONObject getSongData()  {
+        String url = getRandomEndpoint();
+        JSONObject response;
+        try {
+            Request request = new Request.Builder()
+                .url(url)
+                .addHeader("Authorization", "Bearer " + authToken)
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+            Response responseRequest = client.newCall(request).execute();
+
+            if(responseRequest.code() == 401){ // Code 401 - bad (expired or null) auth token
+                generateAuthToken();
+                Request request2 = new Request.Builder()
+                        .url(url)
+                        .addHeader("Authorization", "Bearer " + authToken)
+                        .addHeader("Content-Type", "application/json")
+                        .build();
+                responseRequest = client.newCall(request2).execute();
+            }
+
+            if(responseRequest.body() == null) {throw new RuntimeException("Error - no response body");}
+
+            response = new JSONObject(responseRequest.body().string());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return response;
+    }
+    private String getRandomEndpoint(){
+        String base_endpoint = "https://api.spotify.com/v1/search?q=";
+        char search = (char) ('a' + (new Random()).nextInt(26));
+        return String.format("%s%s&genre:%s&type=track&limit=50", base_endpoint, search, genre);
+    }
+    private Song[] handleGetSongs(){
+
+        ArrayList<Song> songs = new ArrayList<>();
+
+        JSONObject responseBody = getSongData();
+        JSONArray songsArray = responseBody.getJSONObject("tracks").getJSONArray("items");
+
+        for (int i = 0; i < songsArray.length(); i++) {
+            JSONObject item = (JSONObject) songsArray.get(i);
+            JSONObject albumArtistInfo = (JSONObject) item.getJSONObject("album").getJSONArray("artists").get(0);
+            if(item.get("preview_url") instanceof String && item.getInt("popularity") > 73){
+                String songName = item.getString("name");
+                String audio = item.getString("preview_url");
+                String artistName = albumArtistInfo.getString("name");
+                // TODO: Create Song entity
+                // TODO: Add song to list
+            }
+        }
+        return songs.toArray(Song[]::new);
+    }
+
+
+
 }
