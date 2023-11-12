4d34e2f2072af541541a62d03442f823edca764e
ethanmcf <ethan_mcfarland@outlook.com>
2023-11-12 10:27:52 -0500

    Created and implemented input/output data/boundaries and data access interface

---
+++ src/use_case/finish_round/FinishRoundInputData.java
@@ -0,0 +1,10 @@
+package use_case.finish_round;
+
+public class FinishRoundInputData {
+    private final String gameId;
+
+    public FinishRoundInputData(String gameId) {
+        this.gameId = gameId;
+    }
+    String getGameId() { return gameId; }
+}
