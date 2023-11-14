04b3fd42c00b775b40b4ee254b38ee115427d235
Aabha Roy <1aabharoy@gmail.com>
2023-11-14 18:26:48 -0500

    Completed implementation for ToggleAudioState class

--- src/interface_adapter/toggle_audio/ToggleAudioState.java
+++ src/interface_adapter/toggle_audio/ToggleAudioState.java
@@ -1,4 +1,19 @@
 package interface_adapter.toggle_audio;
 
 public class ToggleAudioState {
+    private String imgPath = "";
+
+    public ToggleAudioState(ToggleAudioState copy) {
+        imgPath = copy.imgPath;
+    }
+
+    public ToggleAudioState() {}
+
+    public String getImgPath() {
+        return imgPath;
+    }
+
+    public void setImgPath(String imgPath) {
+        this.imgPath = imgPath;
+    }
 }
