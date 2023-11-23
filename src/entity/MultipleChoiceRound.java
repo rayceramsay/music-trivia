dab180dfc0f9b3970a208488e3ee79fe9f594204
ethanmcf <ethan_mcfarland@outlook.com>
2023-11-23 12:22:36 -0500

    Updated MultipleChoiceRound to include possible options for user to select

---
+++ src/entity/MultipleChoiceRound.java
@@ -0,0 +1,63 @@
+package entity;
+
+import org.checkerframework.checker.units.qual.A;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MultipleChoiceRound implements Round{
+    private final Song song;
+    private final String question;
+    private final String correctAnswer;
+    private String userAnswer = "";
+    private ArrayList<String> incorrectOptions;
+    public MultipleChoiceRound(Song song, String question, String correctAnswer){
+        this.song = song;
+        this.question = question;
+        this.correctAnswer = correctAnswer;
+        this.incorrectOptions = new ArrayList<>();
+
+    }
+    @Override
+    public String getQuestion() {
+        return this.question;
+    }
+
+    @Override
+    public Song getSong() {
+        return this.song;
+    }
+
+    @Override
+    public String getCorrectAnswer() {
+        return this.correctAnswer;
+    }
+
+    @Override
+    public String getUserAnswer() {
+        return this.userAnswer;
+    }
+
+    @Override
+    public void setUserAnswer(String userAnswer) {
+        this.userAnswer = userAnswer;
+    }
+
+    @Override
+    public boolean isUserAnswerCorrect() {
+        String cleanedUserAnswer = cleanString(userAnswer);
+        return cleanedUserAnswer.equalsIgnoreCase(correctAnswer);
+    }
+    public void addIncorrectOptions(ArrayList<String> options){
+        this.incorrectOptions.addAll(options);
+    }
+    public ArrayList<String> getRandomOrderOptions(){
+        ArrayList<String> options = new ArrayList<>(this.incorrectOptions);
+        int rand = new Random().nextInt(0, this.incorrectOptions.size());
+        options.add(rand, this.correctAnswer);
+        return options;
+    }
+    private String cleanString(String string) {
+        return string.replaceAll("\\p{C}", "").trim();
+    }
+}
