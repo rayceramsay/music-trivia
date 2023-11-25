b150fc0db596638b6f0c55eb4278102665dd995a
Rayce Ramsay <ramsayrayce@gmail.com>
2023-11-25 13:55:44 -0500

    Add functionality for saving a game entity to SQLite db
    - Created deleteGame db functionality
    - Created insertNewGame db functionality
    - Tweaked db schema to match simplify functionality

--- src/data_access/SQLiteDatabaseGameDataAccessObject.java
+++ src/data_access/SQLiteDatabaseGameDataAccessObject.java
@@ -1,84 +1,217 @@
 package data_access;
 
+import entity.Game;
+import entity.Round;
+import entity.Song;
 import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.*;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class SQLiteDatabaseGameDataAccessObject {
 
+    private final static DateTimeFormatter sqliteDateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
     private final String databasePath;
     private final String databaseName;
-    private Connection sharedConnection;
 
     public SQLiteDatabaseGameDataAccessObject() {
         Dotenv dotenv = Dotenv.load();
         databasePath = dotenv.get("DB_PATH");
         databaseName = dotenv.get("DB_NAME");
 
-        setupDatabase();
+        try {
+            setupDatabase();
+        } catch (SQLException e) {
+            throw new RuntimeException("Could not setup SQLite database.");
+        }
     }
 
-    private Connection getConnection() {
-        if (sharedConnection != null) {
-            return sharedConnection;
-        }
-
-        String databaseUrl = "jdbc:sqlite:" + databasePath + databaseName + ".db";
-        try {
-            sharedConnection = DriverManager.getConnection(databaseUrl);
-        } catch (SQLException e) {
-            throw new RuntimeException("Could not connect to the database.");
-        }
-
-        return sharedConnection;
-    }
-
-    private void closeConnection() {
-        if (sharedConnection == null) {
-            return;
-        }
+    public void save(Game game) {
+        Connection connection = getConnection();
 
         try {
-            sharedConnection.close();
+            connection.setAutoCommit(false);
+            deleteGame(game, connection);
+            insertNewGame(game, connection, false);
+            connection.commit();
         } catch (SQLException e) {
-            throw new RuntimeException("Could not close connection to the database.");
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+            throw new RuntimeException(e);
         } finally {
-            sharedConnection = null;
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 
-    private void setupDatabase() {
+    private boolean gameExists(Game game, Connection connection) throws SQLException {
+        boolean gameExists = false;
+
+        String sql = "SELECT id FROM game WHERE external_id = ? LIMIT 1";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, game.getID());
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                gameExists = true;
+            }
+        }
+
+        return gameExists;
+    }
+
+    private void deleteGame(Game game, Connection connection) throws SQLException {
+        String deleteGameSql = "DELETE FROM game WHERE external_id = ?";
+
+        try (PreparedStatement statement = connection.prepareStatement(deleteGameSql)) {
+            statement.setString(1, game.getID());
+            statement.executeUpdate();
+        }
+    }
+
+    private void insertNewGame(Game game, Connection connection, boolean useTransaction) throws SQLException {
+        String insertGameSql = """
+            INSERT INTO game (external_id, difficulty, genre, initial_lives, max_rounds, current_lives, score, created_at, finished_at)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """;
+        String insertRoundSql = "INSERT INTO round (game_id, question, correct_answer, user_answer) VALUES (?, ?, ?, ?)";
+        String insertSongSql = "INSERT INTO song (round_id, title, artist, audio_path) VALUES (?, ?, ?, ?)";
+        String insertGameRoundSql = "INSERT INTO game_round (game_id, round_id) VALUES (?, ?)";
+
+        PreparedStatement insertGameStatement = null;
+        PreparedStatement insertSongStatement = null;
+        PreparedStatement insertRoundStatement = null;
+        PreparedStatement insertGameRoundStatement = null;
+
+        try {
+            if (useTransaction) {
+                connection.setAutoCommit(false);
+            }
+
+            insertGameStatement = connection.prepareStatement(insertGameSql);
+            insertGameStatement.setString(1, game.getID());
+            insertGameStatement.setString(2, game.getDifficulty());
+            insertGameStatement.setString(3, game.getGenre());
+            insertGameStatement.setInt(4, game.getInitialLives());
+            insertGameStatement.setInt(5, game.getMaxRounds());
+            insertGameStatement.setInt(6, game.getCurrentLives());
+            insertGameStatement.setInt(7, game.getScore());
+            insertGameStatement.setString(8, game.getCreatedAt().format(sqliteDateFormatter));
+            String finishedAt =  game.getFinishedAt() != null ? game.getFinishedAt().format(sqliteDateFormatter) : null;
+            insertGameStatement.setString(9, finishedAt);
+            insertGameStatement.executeUpdate();
+
+            int internalGameId = getLastInsertedRowid(connection);
+
+            for (Round round : game.getRounds()) {
+                insertRoundStatement = connection.prepareStatement(insertRoundSql);
+                insertRoundStatement.setInt(1, internalGameId);
+                insertRoundStatement.setString(2, round.getQuestion());
+                insertRoundStatement.setString(3, round.getCorrectAnswer());
+                insertRoundStatement.setString(4, round.getUserAnswer());
+                insertRoundStatement.executeUpdate();
+                insertRoundStatement.close();
+
+                int internalRoundId = getLastInsertedRowid(connection);
+
+                Song song = round.getSong();
+                insertSongStatement = connection.prepareStatement(insertSongSql);
+                insertSongStatement.setInt(1, internalRoundId);
+                insertSongStatement.setString(2, song.getTitle());
+                insertSongStatement.setString(3, song.getArtist());
+                insertSongStatement.setString(4, song.getAudio().getPath());
+                insertSongStatement.executeUpdate();
+                insertSongStatement.close();
+
+                insertGameRoundStatement = connection.prepareStatement(insertGameRoundSql);
+                insertGameRoundStatement.setInt(1, internalGameId);
+                insertGameRoundStatement.setInt(2, internalRoundId);
+                insertGameRoundStatement.execute();
+                insertGameRoundStatement.close();
+
+                if (useTransaction) {
+                    connection.commit();
+                }
+            }
+        } catch (SQLException e) {
+            if (useTransaction) {
+                connection.rollback();
+            }
+            throw e;
+        }
+        finally {
+            try {
+                if (insertGameStatement != null) {
+                    insertGameStatement.close();
+                }
+                if (insertSongStatement != null) {
+                    insertSongStatement.close();
+                }
+                if (insertRoundStatement != null) {
+                    insertRoundStatement.close();
+                }
+                if (insertGameRoundStatement != null) {
+                    insertGameRoundStatement.close();
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private int getLastInsertedRowid(Connection connection) throws SQLException {
+        int lastInsertedRowid;
+
+        String getLastInsertedRowidSql = "SELECT last_insert_rowid()";
+
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(getLastInsertedRowidSql);
+            lastInsertedRowid = resultSet.getInt(1);
+        }
+
+        return lastInsertedRowid;
+    }
+
+    private void setupDatabase() throws SQLException {
         String createGameTableSql = """
                 CREATE TABLE IF NOT EXISTS game (
                     id INTEGER PRIMARY KEY,
+                    external_id TEXT NOT NULL UNIQUE,
                     difficulty TEXT NOT NULL,
                     genre TEXT NOT NULL,
                     initial_lives INTEGER NOT NULL,
                     max_rounds INTEGER NOT NULL,
                     current_lives INTEGER NOT NULL,
-                    score INTEGER NOT NULL DEFAULT 0,
-                    created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                    score INTEGER NOT NULL,
+                    created_at TEXT NOT NULL,
                     finished_at TEXT
-                );
-                """;
-        String createSongTableSql = """
-                CREATE TABLE IF NOT EXISTS song (
-                    id INTEGER PRIMARY KEY,
-                    title TEXT NOT NULL,
-                    artist TEXT NOT NULL,
-                    audio_path TEXT NOT NULL
                 );
                 """;
         String createRoundTableSql = """
                 CREATE TABLE IF NOT EXISTS round (
                     id INTEGER PRIMARY KEY,
                     game_id INTEGER NOT NULL,
-                    song_id INTEGER NOT NULL,
                     question TEXT NOT NULL,
                     correct_answer TEXT NOT NULL,
                     user_answer TEXT,
-                    created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                    FOREIGN KEY (game_id) REFERENCES game (id),
-                    FOREIGN KEY (song_id) REFERENCES song (id)
+                    created_at TEXT NOT NULL,
+                    FOREIGN KEY (game_id) REFERENCES game (id) ON DELETE CASCADE
+                );
+                """;
+        String createSongTableSql = """
+                CREATE TABLE IF NOT EXISTS song (
+                    id INTEGER PRIMARY KEY,
+                    round_id INTEGER NOT NULL UNIQUE,
+                    title TEXT NOT NULL,
+                    artist TEXT NOT NULL,
+                    audio_path TEXT NOT NULL,
+                    FOREIGN KEY (round_id) REFERENCES round (id) ON DELETE CASCADE
                 );
                 """;
         String createGameRoundTableSql = """
@@ -86,24 +219,28 @@ public class SQLiteDatabaseGameDataAccessObject {
                     id INTEGER PRIMARY KEY,
                     game_id INTEGER NOT NULL,
                     round_id INTEGER NOT NULL,
-                    FOREIGN KEY (game_id) REFERENCES game (id),
-                    FOREIGN KEY (round_id) REFERENCES round (id)
+                    FOREIGN KEY (game_id) REFERENCES game (id) ON DELETE CASCADE,
+                    FOREIGN KEY (round_id) REFERENCES round (id) ON DELETE CASCADE,
+                    UNIQUE(game_id, round_id)
                 );
                 """;
 
-        Connection connection = getConnection();
-        try {
-            Statement statement = connection.createStatement();
-
-            // Create database tables if they don't exist
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement()) {
             statement.execute(createGameTableSql);
-            statement.execute(createSongTableSql);
             statement.execute(createRoundTableSql);
+            statement.execute(createSongTableSql);
             statement.execute(createGameRoundTableSql);
+        }
+    }
+
+    private Connection getConnection() {
+        String databaseUrl = "jdbc:sqlite:" + databasePath + databaseName + ".db";
+
+        try {
+            return DriverManager.getConnection(databaseUrl);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            closeConnection();
+            throw new RuntimeException("Could not connect to the database.");
         }
     }
 }
