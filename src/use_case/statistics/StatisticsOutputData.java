8ce57b5acfe5afdfb3f07d64f7919aba9795563f
Artak Arakelyan <144293656+Artakarak@users.noreply.github.com>
2023-11-27 20:31:59 -0500

    Update src/use_case/statistics/StatisticsOutputData.java
    Co-authored-by: Ethan McFarland <89097702+ethanmcf@users.noreply.github.com>

--- src/use_case/statistics/StatisticsOutputData.java
+++ src/use_case/statistics/StatisticsOutputData.java
@@ -4,10 +4,60 @@ import java.util.HashMap;
 
 public class StatisticsOutputData {
 
-    private final HashMap<String, Object> stringObjectHashMap;
-    public StatisticsOutputData(HashMap<String, Object> stringObjectHashMap) {
-        this.stringObjectHashMap = stringObjectHashMap;
+    private String commonGameDifficulty;
+    private String commonGameGenre;
+    private int averageLives;
+    private int averageScore;
+    private int averageRoundsPlayed;
+    private boolean hasStats;
+
+
+    public StatisticsOutputData() {}
+    public String getCommonGameDifficulty() {
+        return commonGameDifficulty;
     }
 
-    public HashMap<String, Object> getStats(){return stringObjectHashMap;}
+    public void setCommonGameDifficulty(String commonGameDifficulty) {
+        this.commonGameDifficulty = commonGameDifficulty;
+    }
+
+    public String getCommonGameGenre() {
+        return commonGameGenre;
+    }
+
+    public void setCommonGameGenre(String commonGameGenre) {
+        this.commonGameGenre = commonGameGenre;
+    }
+
+    public int getAverageLives() {
+        return averageLives;
+    }
+
+    public void setAverageLives(int averageLives) {
+        this.averageLives = averageLives;
+    }
+
+    public int getAverageScore() {
+        return averageScore;
+    }
+
+    public void setAverageScore(int averageScore) {
+        this.averageScore = averageScore;
+    }
+
+    public int getAverageRoundsPlayed() {
+        return averageRoundsPlayed;
+    }
+
+    public void setAverageRoundsPlayed(int averageRoundsPlayed) {
+        this.averageRoundsPlayed = averageRoundsPlayed;
+    }
+
+    public boolean hasStats() {
+        return hasStats;
+    }
+
+    public void setHasStats(boolean hasStats) {
+        this.hasStats = hasStats;
+    }
 }
