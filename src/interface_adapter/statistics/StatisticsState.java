770a9b1df2ac2287c0597a003b057391222450d0
Artak Arakelyan <144293656+Artakarak@users.noreply.github.com>
2023-11-27 20:32:27 -0500

    Update src/interface_adapter/statistics/StatisticsState.java
    Co-authored-by: Ethan McFarland <89097702+ethanmcf@users.noreply.github.com>

--- src/interface_adapter/statistics/StatisticsState.java
+++ src/interface_adapter/statistics/StatisticsState.java
@@ -1,58 +1,14 @@
 package interface_adapter.statistics;
 
 public class StatisticsState {
-    public String commonGameDifficulty;
-    public String commonGameGenre;
-    public int averageLives;
-    public int averageScore;
-    public int averageRoundsPlayed;
+   public String message;
 
-    public int getAverageRoundsPlayed() {
-        return averageRoundsPlayed;
-    }
-
-    public void setAverageRoundsPlayed(int averageRoundsPlayed) {
-        this.averageRoundsPlayed = averageRoundsPlayed;
-    }
-
-    public StatisticsState(StatisticsState copy) {
-        this.commonGameDifficulty = copy.commonGameDifficulty;
-        this.commonGameGenre = copy.commonGameGenre;
-        this.averageLives = copy.averageLives;
-        this.averageScore = copy.averageScore;
-        this.averageRoundsPlayed = copy.averageRoundsPlayed;
-    }
     public StatisticsState(){}
-
-    public String getCommonGameDifficulty() {
-        return commonGameDifficulty;
+    public String getStatsMessage() {
+        return message;
     }
 
-    public void setCommonGameDifficulty(String commonGameDifficulty) {
-        this.commonGameDifficulty = commonGameDifficulty;
-    }
-
-    public String getCommonGameGenre() {
-        return commonGameGenre;
-    }
-
-    public void setCommonGameGenre(String commonGameGenre) {
-        this.commonGameGenre = commonGameGenre;
-    }
-
-    public int getAverageLives() {
-        return averageLives;
-    }
-
-    public void setAverageLives(int averageLives) {
-        this.averageLives = averageLives;
-    }
-
-    public int getAverageScore() {
-        return averageScore;
-    }
-
-    public void setAverageScore(int averageScore) {
-        this.averageScore = averageScore;
+    public void setStatsMessage(String message) {
+        this.message = message;
     }
 }
