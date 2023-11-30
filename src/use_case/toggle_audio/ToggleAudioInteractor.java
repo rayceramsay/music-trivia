71ea92f211e385c852c2680e0330fb06f0c62970
Rayce Ramsay <ramsayrayce@gmail.com>
2023-11-30 02:53:13 -0500

    Hacky fix for setting toggle audio button to correct image after audio naturally finishes

--- src/use_case/toggle_audio/ToggleAudioInteractor.java
+++ src/use_case/toggle_audio/ToggleAudioInteractor.java
@@ -1,14 +1,15 @@
 package use_case.toggle_audio;
 
-
 import entity.Game;
 import entity.PlayableAudio;
 import entity.Round;
 import entity.Song;
 
 public class ToggleAudioInteractor implements ToggleAudioInputBoundary {
+
     private final ToggleAudioGameDataAccessInterface toggleAudioDataAccessObject;
     private final ToggleAudioOutputBoundary toggleAudioPresenter;
+    private Thread checkToggleStateThread;
 
     public ToggleAudioInteractor(ToggleAudioGameDataAccessInterface toggleAudioDataAccessObject, ToggleAudioOutputBoundary toggleAudioPresenter) {
         this.toggleAudioDataAccessObject = toggleAudioDataAccessObject;
@@ -27,14 +28,53 @@ public class ToggleAudioInteractor implements ToggleAudioInputBoundary {
         boolean audioPlaying = songAudio.isPlaying();
 
         if (audioPlaying) {
+            cancelTimerForShowingPlayButton();
+
             //stopping audio and showing play button
             songAudio.stop();
             toggleAudioPresenter.showPlayButton();
         }
         else {
-            //playing audio and showing pause button
+            startTimerForShowingPlayButton(songAudio);
+
+            // playing audio and showing pause button
             songAudio.play();
             toggleAudioPresenter.showPauseButton();
         }
+    }
+
+    private void startTimerForShowingPlayButton(PlayableAudio audio) {
+        cancelTimerForShowingPlayButton();
+
+        checkToggleStateThread = new Thread(() -> {
+            try {
+                int timeBetweenChecks = 50;
+                int audioMaxDuration = 10000;
+                int buffer = 100;
+
+                int i = 0;
+                while (i < audioMaxDuration + buffer) {
+                    Thread.sleep(timeBetweenChecks);
+                    if (Thread.interrupted()) {
+                        return;
+                    }
+                    i += timeBetweenChecks;
+                }
+
+                if (!audio.isPlaying()) {
+                    toggleAudioPresenter.showPlayButton();
+                }
+            } catch (InterruptedException ignored) {
+            }
+        });
+        checkToggleStateThread.start();
+    }
+
+    private void cancelTimerForShowingPlayButton() {
+        if (checkToggleStateThread == null) {
+            return;
+        }
+
+        checkToggleStateThread.interrupt();
     }
 }
