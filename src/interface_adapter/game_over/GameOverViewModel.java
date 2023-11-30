22a9c94ad86aaec637917ff65cfdbc1824affab2
Rayce Ramsay <ramsayrayce@gmail.com>
2023-11-29 20:38:04 -0500

    Delete play again button from game over view

--- src/interface_adapter/game_over/GameOverViewModel.java
+++ src/interface_adapter/game_over/GameOverViewModel.java
@@ -6,10 +6,9 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 public class GameOverViewModel extends ViewModel {
-    public final String TITLE_LABEL = "GAME OVER!";
 
+    public static final String TITLE_LABEL = "GAME OVER!";
     public static final String MAIN_MENU_LABEL = "Go to main menu";
-    public static final String PLAY_AGAIN_LABEL = "Play again";
 
     private GameOverState state = new GameOverState();
 
