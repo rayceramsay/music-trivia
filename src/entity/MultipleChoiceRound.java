5a3d3370e51d7ae86588ad4aa91cbdfac09f19e3
Rayce Ramsay <ramsayrayce@gmail.com>
2023-11-29 21:52:50 -0500

    Modify addIncorrectOptions to use  List instead of ArrayList in MultipleChoiceRound

--- src/entity/MultipleChoiceRound.java
+++ src/entity/MultipleChoiceRound.java
@@ -1,20 +1,24 @@
 package entity;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
-public class MultipleChoiceRound implements Round{
+public class MultipleChoiceRound implements Round {
+
     private final Song song;
     private final String question;
     private final String correctAnswer;
     private String userAnswer;
-    private ArrayList<String> incorrectOptions;
+    private final List<String> incorrectOptions;
+
     public MultipleChoiceRound(Song song, String question, String correctAnswer){
         this.song = song;
         this.question = question;
         this.correctAnswer = correctAnswer;
         this.incorrectOptions = new ArrayList<>();
     }
+
     @Override
     public String getQuestion() {
         return this.question;
@@ -50,16 +54,16 @@ public class MultipleChoiceRound implements Round{
         return cleanedUserAnswer.equalsIgnoreCase(correctAnswer);
     }
 
-    public void addIncorrectOptions(ArrayList<String> options){
+    public void addIncorrectOptions(List<String> options){
         this.incorrectOptions.addAll(options);
     }
-    public ArrayList<String> getRandomOrderOptions(){
+
+    public List<String> getRandomOrderOptions(){
         ArrayList<String> options = new ArrayList<>(this.incorrectOptions);
         int rand = new Random().nextInt(0, this.incorrectOptions.size());
         options.add(rand, this.correctAnswer);
         return options;
     }
-
 
     @Override
     public boolean isFinished() {
